dc8375631ed4816db910f2d1512ebf5c305a4e46
Dan Oved <stangogh@gmail.com>
2022-06-25 00:43:56 -0400

    having scene render

---
+++ packages/react-app/src/Scene/Elements/ElementsTree.tsx
@@ -0,0 +1,13 @@
+import { SceneConfiguration } from "../Config/types/scene";
+import Element from './Element';
+
+const ElementsTree = ({elements}: Pick<SceneConfiguration, 'elements'>) => {
+  if (!elements) return null;
+  return <>
+  {elements.map((element, i) => (
+    <Element config={element} />
+  ))}
+  </>
+}
+
+export default ElementsTree;
--- packages/react-app/src/Scene/Root.tsx
+++ packages/react-app/src/Scene/Root.tsx
@@ -2,6 +2,7 @@ import { Canvas } from "@react-three/fiber";
 import ErrorBoundary from "../shared/ErrorBoundary";
 import { SceneConfiguration } from "./Config/types/scene";
 import Controls from "./Controls";
+import ElementsTree from "./Elements/ElementsTree";
 import Environment from "./Elements/Environment";
 
 const Root = ({sceneConfig}:{
@@ -10,6 +11,7 @@ const Root = ({sceneConfig}:{
   return <Canvas>
     <ErrorBoundary>
        <Environment environment={sceneConfig.environment}/>
+     <ElementsTree elements={sceneConfig.elements} />
      <Controls />
      </ErrorBoundary>
   </Canvas>
